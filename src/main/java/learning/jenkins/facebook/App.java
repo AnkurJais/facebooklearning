6f7e58ad3ae2c7b9f28def81761999551173cf4d
ankur <aloha.ankur@gmail.com>
2017-11-04 18:13:07 +0530

    Initial Commit

---
+++ src/main/java/learning/jenkins/facebook/App.java
@@ -0,0 +1,14 @@
+package learning.jenkins.facebook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        TestNGLauncher testng = new TestNGLauncher();
+        testng.invokeTestNG();
+    }
+}
---
+++ src/main/java/learning/jenkins/pageobjects/HomePageObjects.java
@@ -0,0 +1,15 @@
+package learning.jenkins.pageobjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePageObjects {
+	@FindBy(xpath="//a/span[contains(text(),'Leonardo')]")
+	public WebElement user_profile;
+	
+	@FindBy(id="pass")
+	public WebElement pass;
+	
+	@FindBy(xpath="//div/a/span[contains(text(),Leonardo)]/parent::a")
+	public WebElement user_profile_anchor;	
+}
