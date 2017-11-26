31a8e256a5f639e147c9da39895dc936149396bf
ankur <ankur.javatm@gmail.com>
2017-11-26 17:36:36 +0530

    Updated Driver.java

--- src/main/java/learning/jenkins/facebook/Driver.java
+++ src/main/java/learning/jenkins/facebook/Driver.java
@@ -1,8 +1,13 @@
 package learning.jenkins.facebook;
 
+import org.junit.Test;
+
 public class Driver {
 
-	public static void main(String[] args){
-		
-	}
+    public static void main( String[] args )
+    {
+    	System.out.println(args[0]+args[1]);
+    	TestNGLauncher testng = new TestNGLauncher();
+        testng.invokeTestNG();
+    }  
 }
